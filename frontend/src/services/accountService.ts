5c5099ca65f6c797edc29d56755488b5c3eb579d
22127304 <ntnhan221@clc.fitus.edu.vn>
2024-12-28 22:39:49 +0700

    fix

--- frontend/src/services/accountService.ts
+++ frontend/src/services/accountService.ts
@@ -3,9 +3,12 @@ import { type AccountResponse } from "../types/accountType";
 
 export const getAccounts = async (
 	page = 1,
-	per_page = 10
+	per_page = 10,
+	q = ""
 ): Promise<AccountResponse | null> => {
-	const response = await fetch(API + `?page=${page}&per_page=${per_page}`);
+	const response = await fetch(
+		API + `?page=${page}&per_page=${per_page}&q=${q}`
+	);
 	if (response.status !== 200) return null;
 
 	return await response.json();
