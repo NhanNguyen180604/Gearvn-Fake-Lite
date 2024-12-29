1cb7815ac5a9ddcd12bd16ab0a07f7bd95b85f51
22127304 <ntnhan221@clc.fitus.edu.vn>
2024-12-29 20:40:52 +0700

    get token from user header and prop drilling to components

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,6 +1,36 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref, onMounted, watch } from 'vue';
 import userHeader from '../components/userHeader.vue'
+import { useRouter } from "vue-router";
+import { useUser, useAuth, useSession } from '@clerk/vue';
+
+const { getToken } = useAuth();
+const { session } = useSession();
+
+const router = useRouter();
+
+const { user } = useUser();
+const token = ref('');
+
+watch(user, async () => {
+    await initialize();
+});
+onMounted(async () => {
+    await initialize();
+});
+
+const initialize = async () => {
+    if (user.value) {
+        // navigate admin to their page
+        if (user.value.publicMetadata.role === 'admin')
+            router.push('/admin/products');
+
+        // get user token to pass to pages
+        if (session.value)
+            token.value = await session.value.getToken({ template: 'test-template' });
+        else token.value = await getToken.value({ template: 'test-template' });
+    }
+};
 
 </script>
 
@@ -8,9 +38,11 @@ import userHeader from '../components/userHeader.vue'
     <div style="background-color: var(--background-gray)">
         <userHeader />
 
-        <RouterView />
+        <RouterView v-slot="{ Component, route }">
+            <component :is="Component" :token="token" />
+        </RouterView>
 
-        
+
     </div>
 </template>
 
